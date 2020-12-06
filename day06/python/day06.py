a9f55f934677390bec056c324ce924eb770aab7a
shandrayu <yuliia.shandra@gmail.com>
2020-12-06 22:28:44 +0200

    Day 6 part 2 python solution

--- day06/python/day06.py
+++ day06/python/day06.py
@@ -15,10 +15,31 @@ def count_anyone_answered_yes(lines: List[str]) -> int:
     counts.append(len(current_questions))
     return sum(counts)
 
+def count_everyone_answered_yes(lines: List[str]) -> int:
+    counts = []
+    current_questions = set()
+    is_first_person = True
+    for line in lines:
+        if line == '\n':
+            counts.append(len(current_questions))
+            current_questions = set()
+            is_first_person = True
+        else:
+            line = line.strip('\n')
+            answers = set(line)
+            if is_first_person:
+                current_questions = answers
+                is_first_person = False
+            else:
+                current_questions = current_questions.intersection(answers)
+    # last set of questions
+    counts.append(len(current_questions))
+    return sum(counts)
 
 if __name__ == '__main__':
     counts = []
     with open('day06/input.txt') as f:
         lines = f.readlines()
     
-    print('Sum of the counts is {}'.format(count_anyone_answered_yes(lines)))
+    print('Anyone: Sum of the counts is {}'.format(count_anyone_answered_yes(lines)))
+    print('Everyone: Sum of the counts is {}'.format(count_everyone_answered_yes(lines)))
