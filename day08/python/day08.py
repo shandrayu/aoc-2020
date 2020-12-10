01493223368e1694cfdb801a2fa965bf7cf950f4
shandrayu <yuliia.shandra@gmail.com>
2020-12-10 22:00:23 +0200

    Day 8 part 2 python solution

--- day08/python/day08.py
+++ day08/python/day08.py
@@ -23,10 +23,16 @@ def read_command(line: str) -> Command:
     command_type_str, value_str = line.split(' ') 
     return Command(get_type(command_type_str), int(value_str))
 
-def execute(program) -> int:
+def reset(program):
+    for command in program:
+        command[1] = False
+
+def execute(program) -> (int, bool):
     acc = 0
     idx = 0
-    while not program[idx][1]:
+    reset(program)
+
+    while idx < len(program) and not program[idx][1]:
         command = program[idx][0]
         program[idx][1] = True
         if command.command_type == CommandType.ACC:
@@ -36,7 +42,23 @@ def execute(program) -> int:
             idx += 1
         elif command.command_type == CommandType.JMP:
             idx += command.value
-    return acc
+
+    has_terminated = idx == len(program)
+
+    return acc, has_terminated
+
+def get_mutated_program():
+    idx = 0
+    while idx < len(program):
+        if program[idx][0].command_type == CommandType.JMP:
+            program[idx][0] = Command(CommandType.NOP, program[idx][0].value)
+            yield program
+            program[idx][0] = Command(CommandType.JMP, program[idx][0].value)
+        elif program[idx][0].command_type == CommandType.NOP:
+            program[idx][0] = Command(CommandType.JMP, program[idx][0].value)
+            yield program
+            program[idx][0] = Command(CommandType.NOP, program[idx][0].value)
+        idx += 1
 
 if __name__ == '__main__':
     program = []
@@ -47,4 +69,18 @@ if __name__ == '__main__':
             command = read_command(line)
             program.append([command, False])
     
-    print('Accumulator value before going into the infinite loop was {} '.format(execute(program)))
+    accumlator_value, has_terminated = execute(program)
+    print('Program execution terminated {}'.format(has_terminated))
+    print('Accumulator value before going into the infinite loop was {} '.format(accumlator_value))
+
+    program_mutator = get_mutated_program()
+    accumulator = -1000
+    current_program = program
+    while current_program:
+        accumulator, has_terminated = execute(current_program)
+        if has_terminated:
+            break
+        current_program = next(program_mutator, False)
+    
+    print('Program has terminated {}'.format(has_terminated))
+    print('Mutated program last accumulator value is {}'.format(accumulator))
